388447e181542b3125cf02056a30832025538dda
joseluislazcanor <joseluislazcanor@gmail.com>
2021-09-14 19:18:06 -0300

    Se eliminaron los archivos que no son del cliente para futuro merge sin complicaciones

--- js/script_css_login.js
+++
@@ -1,8 +0,0 @@
-//Function login
-function iniciarSesion(){
-    if(document.getElementById("txtLoginEmail").value == "admin@admin.com" && document.getElementById("txtLoginPassword").value == "admin"){
-        alert("Usuario y contraseña verificados correctamente");
-    }else{
-        alert("Combinación de credenciales incorrecta");
-    }
-}
