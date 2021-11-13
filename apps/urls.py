b289f04d5376d5b17fa232ab012c3cc32a06757b
Cristian Vasquez <vasquezcamposc@gmail.com>
2021-11-13 16:52:31 -0300

    cambio en html garzon

--- apps/urls.py
+++ apps/urls.py
@@ -42,7 +42,7 @@ urlpatterns =[
     #HTML GARZON
     path('main_garzon', views.main_garzon, name='main_garzon'),
     path('retiro_platos', views.retiro_platos, name='retiro_platos'),
-    path('ver_reservas', views.ver_reservas, name='ver_reservas'),
+    path('ver_reservaciones', views.ver_reservaciones, name='ver_reservaciones'),
 
     #HTML COCINERO
     path('index_cocina', views.index_cocina, name='index_cocina'),
