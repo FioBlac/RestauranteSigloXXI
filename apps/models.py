14cd54284fb63775e4a485d319dd026fbf5cdf03
Cristian Vasquez <vasquezcamposc@gmail.com>
2021-11-27 20:05:17 -0300

    cambio en models plato y pedido

--- apps/models.py
+++ apps/models.py
@@ -201,6 +201,8 @@ class Pedido(models.Model):
     reserva_id_reserva = models.ForeignKey('Reserva', models.DO_NOTHING, db_column='reserva_id_reserva', blank=True, null=True,related_name='+')
     plato_id_plato = models.ForeignKey('Plato', models.DO_NOTHING, db_column='plato_id_plato', blank=True, null=True,related_name='+')
     entrega_id_entrega = models.ForeignKey(Entrega, models.DO_NOTHING, db_column='entrega_id_entrega', blank=True, null=True,related_name='+')
+    estado = models.CharField(max_length=30)
+    detalle = models.CharField(max_length=200)
 
     class Meta:
         managed = False
@@ -212,7 +214,7 @@ class Plato(models.Model):
     nombre_plato = models.CharField(max_length=40)
     precio = models.FloatField()
     tipo_plato = models.CharField(max_length=50)
-    tiempo_prepar = models.DateField()
+    tiempo_prepar = models.CharField(max_length=6)
     foto_plato = models.ImageField(upload_to='img',
                             db_column='foto_plato',
                             blank=True)
