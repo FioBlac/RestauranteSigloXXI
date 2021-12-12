30b6e99983669c1ab0e0140a6cf855a8b728ad0b
Cristian Vasquez <vasquezcamposc@gmail.com>
2021-12-12 18:23:55 -0300

    cambios en navbar de index

--- orden/views.py
+++ orden/views.py
@@ -15,69 +15,104 @@ from django.contrib.auth.models import User
 
 
 class OrdenViews(LoginRequiredMixin, ListView):
-   login_url = 'login'
-   template_name ='orden/ordenes.html'
+    login_url = 'login'
+    template_name ='orden/ordenes.html'
 
-   def get_queryset(self):
-      return self.request.user.ordenes_completadas()
+    def get_queryset(self):
+        return self.request.user.ordenes_completadas()
 
 
 
 @login_required(login_url='login')
 def orden (request):
-   cart = funcionCarrito(request)
-   orden = funcionOrden(cart, request)
+    storage = messages.get_messages(request)
+    for _ in storage:
+        # This is important
+        # Without this loop _loaded_messages is empty
+        pass
 
-   return render(request,'orden/orden.html',{
-   'cart': cart,
-   'orden':orden,
-   'breadcrumb': breadcrumb()
-   })
+    for _ in list(storage._loaded_messages):
+        del storage._loaded_messages[0]
+
+    cart = funcionCarrito(request)
+    orden = funcionOrden(cart, request)
+
+    return render(request,'orden/orden.html',{
+    'cart': cart,
+    'orden':orden,
+    'breadcrumb': breadcrumb()
+    })
 
 @login_required(login_url='login')
 def confirmacion(request):
-   cart=funcionCarrito(request)
-   orden=funcionOrden(cart, request)
-   
+    storage = messages.get_messages(request)
+    for _ in storage:
+        # This is important
+        # Without this loop _loaded_messages is empty
+        pass
+
+    for _ in list(storage._loaded_messages):
+        del storage._loaded_messages[0]
+
+    cart=funcionCarrito(request)
+    orden=funcionOrden(cart, request)
 
 
-   return render(request,'orden/confirmacion.html',{
-      'cart':cart,
-      'orden':orden,
-      'breadcrumb':  breadcrumb(confirmation=True),
 
-   })
+    return render(request,'orden/confirmacion.html',{
+        'cart':cart,
+        'orden':orden,
+        'breadcrumb':  breadcrumb(confirmation=True),
+    })
 
 @login_required(login_url='login')   
 def cancelar_orden(request):
-   cart=funcionCarrito(request)
-   orden=funcionOrden(cart,request)
+    storage = messages.get_messages(request)
+    for _ in storage:
+        # This is important
+        # Without this loop _loaded_messages is empty
+        pass
+
+    for _ in list(storage._loaded_messages):
+        del storage._loaded_messages[0]
+        
+    cart=funcionCarrito(request)
+    orden=funcionOrden(cart,request)
 
 
-   if request.user.id != orden.user_id:
-      return redirect('index')
+    if request.user.id != orden.user_id:
+            return redirect('index_productos')
 
-   orden.cancelar()
-   deleteCart(request)
-   deleteOrden(request)
+    orden.cancelar()
+    deleteCart(request)
+    deleteOrden(request)
 
-   messages.error(request,'Orden eliminada correctamente')
-   return redirect('index')
+    messages.error(request,'Orden eliminada correctamente')
+    return redirect('index')
 
 @login_required(login_url='login') 
 def completado(request):
-   cart=funcionCarrito(request)
-   orden = funcionOrden(cart, request)
+    storage = messages.get_messages(request)
+    for _ in storage:
+        # This is important
+        # Without this loop _loaded_messages is empty
+        pass
 
-   if request.user.id != orden.user_id:
-      return redirect('index')
+    for _ in list(storage._loaded_messages):
+        del storage._loaded_messages[0]
+        
+    cart=funcionCarrito(request)
+    orden = funcionOrden(cart, request)
 
-   orden.completado()
+    if request.user.id != orden.user_id:
+        return redirect('index_productos')
 
-   deleteCart(request)
-   deleteOrden(request)
+    orden.completado()
 
-   messages.success(request,'Compra realizada ')
-   return redirect('index')
+    deleteCart(request)
+    deleteOrden(request)
+
+    messages.success(request,'Compra realizada ')
+    return redirect('index_productos')
 
 
