b1266bdce43b7ebfc468dca5a396ccce3579ae0f
Cristian Vasquez <vasquezcamposc@gmail.com>
2021-12-12 13:50:44 -0300

    inicio manual de usuario

--- apps/views.py
+++ apps/views.py
@@ -768,6 +768,9 @@ def sendEmailRegistro(email):
 def correo(request):
     return render (request, 'html/general/correo.html')
 
+def manual(request):
+    return render (request, 'html/general/manual.html')
+
 
 #HTML PAGAR
 def metodo_pago(request):
