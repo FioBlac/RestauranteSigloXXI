1a4a176e6b011e50f9bf4fff7e4d329ad0aaf7c2
john861c <john.r861c@gmail.com>
2021-12-19 14:46:07 -0300

    correccion error  listar_orden.html

--- restaurant21/settings.py
+++ restaurant21/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': '127.0.0.1:1521/orcl',
-        'USER': 'rest21',
+        'USER': 'DML',
         'PASSWORD': 'oracle',
         'TEST': {
             'USER': 'default_test',
